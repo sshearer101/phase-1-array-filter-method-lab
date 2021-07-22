5ee3e0f5eb08597d91b027b3e23a2779e4504b7f
Sam Shearer <sshearer101@gmail.com>
2021-07-22 16:01:24 -0600

    commit tree df277adbb23614477df359c82750316a7d4a18ce

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(el => el.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(el => el.startsWith(letters))
+}
+
+function matchName(drivers, match){
+    return drivers.filter(el => el.name === match)
+}
